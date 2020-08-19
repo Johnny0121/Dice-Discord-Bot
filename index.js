e0fcdca6ce65267a366ee6d573ce857653417596
Johnny Le <contact.lejohnny@gmail.com>
2020-08-19 12:38:30 +0100

    Added error handling for incorrectly formatted roll calls

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return;
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
